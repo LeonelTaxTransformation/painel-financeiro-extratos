23869e0c011c216f65bc80620ff5bb0032e7ca5f
Leonel Ugarteche Vargas <leonel.u.vargas@gmail.com>
2026-03-22 00:34:25 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,22 +1,52 @@
 window.APP_DATA = {
-  labels: ["2024-01","2024-02","2024-03","2024-04"],
+  labels: ["2019-08", "2020-08", "2021-08", "2022-08", "2024-04", "2024-12", "2025-04", "2025-08"],
   series: [
     {
-      company: "Empresa A",
-      data: [120000, 110000, 130000, 140000]
+      company: "FOME FOOD LANCHONETE LTDA",
+      data: [112474.84, 203625.97, null, null, null, null, null, null]
     },
     {
-      company: "Empresa B",
-      data: [80000, 90000, 95000, 100000]
+      company: "INFINITY COMERCIO DE LANCHES L",
+      data: [null, null, null, null, 0.0, 32959.32, 147396.47, 5937.36]
+    },
+    {
+      company: "VERO PIAZZA BAR LTDA",
+      data: [null, null, 222342.05, 889479.25, null, null, null, null]
     }
   ],
   details: {
-    "Empresa A": {
+    "FOME FOOD LANCHONETE LTDA": {
+      "2019-08": [
+        { date: "01/08", description: "Apl Aplic Aut Mais", section: "Lançamentos", value: -9746.30 },
+        { date: "01/08", description: "Apl Aplic Aut Mais", section: "Lançamentos", value: -2471.79 },
+        { date: "01/08", description: "Apl Aplic Aut Mais", section: "Lançamentos", value: -27774.91 },
+        { date: "01/08", description: "Saldo final do extrato", section: "Resumo", value: 112474.84 }
+      ],
+      "2020-08": [
+        { date: "01/08", description: "Movimentações bancárias", section: "Lançamentos", value: 203625.97 }
+      ]
+    },
+    "INFINITY COMERCIO DE LANCHES L": {
       "2024-04": [
-        {date:"01/04/2024", description:"Venda", section:"Receita", value:50000},
-        {date:"02/04/2024", description:"Fornecedor", section:"Despesa", value:-20000}
+        { date: "30/04", description: "Saldo final do extrato", section: "Resumo", value: 0.0 }
+      ],
+      "2024-12": [
+        { date: "31/12", description: "Saldo final do extrato", section: "Resumo", value: 32959.32 }
+      ],
+      "2025-04": [
+        { date: "30/04", description: "Saldo final do extrato", section: "Resumo", value: 147396.47 }
+      ],
+      "2025-08": [
+        { date: "31/08", description: "Saldo final do extrato", section: "Resumo", value: 5937.36 }
+      ]
+    },
+    "VERO PIAZZA BAR LTDA": {
+      "2021-08": [
+        { date: "31/08", description: "Saldo final do extrato", section: "Resumo", value: 222342.05 }
+      ],
+      "2022-08": [
+        { date: "31/08", description: "Saldo final do extrato", section: "Resumo", value: 889479.25 }
       ]
     }
-  },
-  generatedAt: "2026-03-21"
+  }
 };
