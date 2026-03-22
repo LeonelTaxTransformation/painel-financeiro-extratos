1679d288806d2b4913b06154353ae24e17a13f8d
Leonel Ugarteche Vargas <leonel.u.vargas@gmail.com>
2026-03-21 23:50:55 -0300

    Add data.js

---
+++ data.js
@@ -0,0 +1,22 @@
+window.APP_DATA = {
+  labels: ["2024-01","2024-02","2024-03","2024-04"],
+  series: [
+    {
+      company: "Empresa A",
+      data: [120000, 110000, 130000, 140000]
+    },
+    {
+      company: "Empresa B",
+      data: [80000, 90000, 95000, 100000]
+    }
+  ],
+  details: {
+    "Empresa A": {
+      "2024-04": [
+        {date:"01/04/2024", description:"Venda", section:"Receita", value:50000},
+        {date:"02/04/2024", description:"Fornecedor", section:"Despesa", value:-20000}
+      ]
+    }
+  },
+  generatedAt: "2026-03-21"
+};
